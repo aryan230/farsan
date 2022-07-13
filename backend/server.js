961630e8b3d1259ecb9e65caf7c4a8948045ffc2
Aryan Agarwal <aryan23062001@gmail.com>
2022-07-14 00:11:11 +0530

    major

--- backend/server.js
+++ backend/server.js
@@ -67,7 +67,7 @@ app.post("/api/create-order", async (req, res) => {
 
 const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.use(express.static(path.join(__dirname, "../frontend", "build")));
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"))
   );
